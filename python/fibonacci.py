c15900cdcf1a1f12712d1d8a5f06d9c1bb2ebbca
Tom <prete316@gmail.com>
2020-12-05 10:38:08 -0600

    Update fibonacci.py

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,19 +1,3 @@
 def fibonacci(n):
-    a = 0
-    b = 1
-    
-    if (n == 0):
-        return a
-    elif (n == 1):
-        return b
 
-    f = 0
-
-    while (n > 1):
-        f = a + b
-        a = b
-        b = f
-        n -= 1
-
-    return f
 
