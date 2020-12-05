f2610507e65c8ac2cefa9ea321541b583809cc75
Tom <prete316@gmail.com>
2020-12-05 10:39:13 -0600

    Update fibonacci.js

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,22 +1,5 @@
-exports.fibonacci = function(num) 
-{
-    let a = 0
-    let b = 1
+const fibonacci = (num) => {
     
-    if (num == 0) 
-        return a
-    else if (num == 1)
-        return b
+}
 
-    let f = 0
-
-    for (let i = 1; i < num; ++i)
-    {
-        f = a + b
-        a = b
-        b = f
-    }
-
-    return f
-};
-
+module.exports = fibonacci
