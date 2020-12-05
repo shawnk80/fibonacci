73580a82433ac43030aa171c41ef7a57e61b8243
Ankur Shah <ankur@codeplatoon.org>
2020-12-05 02:20:07 -0600

    completed python solution

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,2 +1,19 @@
 def fibonacci(n):
-    pass # Your code here
+    a = 0
+    b = 1
+    
+    if (n == 0):
+        return a
+    elif (n == 1):
+        return b
+
+    f = 0
+
+    while (n > 1):
+        f = a + b
+        a = b
+        b = f
+        n -= 1
+
+    return f
+
