f72ed3690435601d2602d5655765244cc73db632
Ankur Shah <ankur@codeplatoon.org>
2020-12-05 00:52:45 -0600

    completed javascript solution

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,3 +1,22 @@
-var fib = require("./fibonacci");
+exports.fibonacci = function(num) 
+{
+    let a = 0
+    let b = 1
+    
+    if (num == 0) 
+        return a
+    else if (num == 1)
+        return b
 
-// put your tests here
+    let f = 0
+
+    for (let i = 1; i < num; ++i)
+    {
+        f = a + b
+        a = b
+        b = f
+    }
+
+    return f
+};
+
