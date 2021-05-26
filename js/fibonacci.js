3a9cd26cd339ed10f5fc3fc8cb524af598139a74
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-26 10:09:52 -0500

    Copy of Shawn and Stephen solution

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,17 @@
+
 const fibonacci = (num) => {
-    
+
+
+  // base cases 0 or 1
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+  // recursive call F(n-1) + F(n-2)
+  return fibonacci(num - 1) + fibonacci(num - 2)
+
 }
 
 module.exports = {fibonacci}
